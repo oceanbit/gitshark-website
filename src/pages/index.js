e6a7b7474fa645199c06ef63991f5f904229f879
Corbin Crutchley <crutchcorn@gmail.com>
2020-04-30 15:09:58 -0700

    Added mailing list to the bottom of the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { SharkBackground } from "../components/shark-background"
 import { ThemeProvider } from "../constants/theme-context"
 import "./styles.scss"
 import { DarkLightSelector } from "../components/dark-light-selector"
-import Button from "@material-ui/core/Button"
 import { OneToOneRatio } from "../components/one-to-one-ratio"
 
 import RepoSVG from "../assets/repository_list.svg"
@@ -15,7 +14,7 @@ import GitHubLinkedSVG from "../assets/settings_github_linked.svg"
 import GitHubSVG from "../assets/github.svg"
 import { SeperateToOverlapImages } from "../components/seperate-to-overlap-images/seperate-to-overlap-images"
 import { AnimatedLine } from "../components/animated-line/animated-line"
-import { Checkbox } from "../components/checkbox"
+import { MailingList } from "../components/mailing-list/mailing-list"
 
 const IndexPage = () => (
   <ThemeProvider>
@@ -43,89 +42,8 @@ const IndexPage = () => (
           <br />
           You can sign up to receive alerts when we have something to share.
         </p>
-        <p id={"interestedPlatforms"} className={"checkboxTitle"}>
-          Platforms you’re interested in:
-        </p>
-        <form
-          action="https://app.convertkit.com/forms/1340347/subscriptions"
-          method="post"
-          data-sv-form="1340347"
-          data-uid="f0cde5565a"
-          data-version="5"
-          data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"facebook":null,"segment":null,"pinterest":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
-        >
-          <fieldset
-            data-group="checkboxes"
-            className="formkit-7770"
-            group="field"
-            type="Custom"
-            order="1"
-            save_as="Tag"
-          >
-            <ul
-              className={"checkboxContainer"}
-              role={"list"}
-              aria-describedby={"interestedPlatforms"}
-            >
-              <li>
-                <Checkbox
-                  label={"Android"}
-                  inputProps={{
-                    id: "tag-7770-1409722",
-                    type: "checkbox",
-                    name: "tags[]",
-                    value: "1409722",
-                  }}
-                />
-              </li>
-              <li>
-                <Checkbox
-                  label={"iOS"}
-                  inputProps={{
-                    id: "tag-7770-1409721",
-                    type: "checkbox",
-                    name: "tags[]",
-                    value: "1409721",
-                  }}
-                />
-              </li>
-              <li>
-                <Checkbox
-                  label={"Windows"}
-                  inputProps={{
-                    id: "tag-7770-1409723",
-                    type: "checkbox",
-                    name: "tags[]",
-                    value: "1409723",
-                  }}
-                />
-              </li>
-              <li>
-                <Checkbox
-                  label={"macOS"}
-                  inputProps={{
-                    id: "tag-7770-1409725",
-                    type: "checkbox",
-                    name: "tags[]",
-                    value: "1409725",
-                  }}
-                />
-              </li>
-            </ul>
-          </fieldset>
-          <div className="signupInputContainer">
-            <input
-              name="email_address"
-              placeholder={"Email address"}
-              required=""
-              type="email"
-              className={"signupInput"}
-            />
-            <Button className="signupBtn" data-element="submit" type={"submit"}>
-              Sign up
-            </Button>
-          </div>
-        </form>
+        <MailingList/>
+        <p className="dontWorry">Don’t worry, we hate spam more than you do.</p>
         <div className="screenshotRow">
           <div className="repositories">
             <OneToOneRatio>
@@ -187,6 +105,8 @@ const IndexPage = () => (
           <h2 className={"featureTitle"}>Dark mode</h2>
           <p className="featureBody">Go ahead. Try it.</p>
         </div>
+        <hr/>
+        <MailingList className="bottomMailing"/>
       </div>
     </div>
   </ThemeProvider>
