1740741c339115bc4e75d525ffd686c9fa66ed04
Corbin Crutchley <crutchcorn@gmail.com>
2020-07-25 15:42:57 -0700

    Added terms and privacy links to homepage

--- src/components/header-sheet/header-sheet.js
+++ src/components/header-sheet/header-sheet.js
@@ -1,25 +1,31 @@
 import { SharkBackground } from "../shark-background"
 import { PlayPauseBackground } from "../play-pause-background"
+import * as styles from "./header-sheet.module.scss"
 import React from "react"
 
 export const HeaderSheet = ({children, isBGPaused, setIsBGPaused}) => {
   return (
     <div style={{ minHeight: "100vh" }}>
-      <div className={"sharkBackground"}>
+      <div className={styles.sharkBackground}>
         <SharkBackground isBGPaused={isBGPaused}/>
       </div>
-      <div className={"sharkHeader"}>
+      <div className={styles.sharkHeader}>
         <img src="/shark_header.png" alt={"GitShark"}/>
         <PlayPauseBackground
-          className={"playPauseIndexBtn"}
+          className={styles.playPauseIndexBtn}
           isPaused={isBGPaused}
           toggle={() =>
             setIsBGPaused(v => !v)
           }
         />
       </div>
-      <div className="bodySheet">
+      <div className={styles.bodySheet}>
         {children}
+        <div className={styles.termSheet}>
+          <a href={"/"}>Home</a>
+          <a href={"/terms"}>Terms and Conditions</a>
+          <a href={"/privacy"}>Privacy Policy</a>
+        </div>
       </div>
     </div>
   )
